48357b5cc34a198afa8c4bd0c514dda96a810b03
Michael07220823 <michael31703@gmail.com>
2021-01-09 10:27:38 +0800

    Close get_now_time method time display.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="new_timer",
-    version="0.0.2",
+    version="0.0.4",
     author="Overcomer",
     author_email="newjerusalem0722@gmail.com",
     description="Program timer.",
